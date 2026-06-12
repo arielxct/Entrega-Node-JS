c9f0ffd7f95ad67695296f9eef83ec9f5efabf79
arielxct <campostula@gmail.com>
2026-06-12 01:11:59 -0300

    Modifico Index de nuevo  para Vercel

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ import express from "express";
 import cors from "cors";
 import admin from "firebase-admin";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
 
 // Inicializar Firebase Admin con variables de entorno
 admin.initializeApp({
